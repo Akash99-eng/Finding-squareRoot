767f28f3bcbd6159bf32d6ba7afe909ce0482810
Akash99-eng <80410694+Akash99-eng@users.noreply.github.com>
2021-04-14 10:16:01 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,22 @@
+#include<math.h>
+#include <stdio.h>
+
+int main()
+{
+    
+int t;
+scanf("%d",&t);
+while(t--){
+
+
+int squareRoot;
+int numbers;
+printf("Enter any numbers to find squareRoot\n");
+scanf("%d",&numbers);
+squareRoot =sqrt(numbers);
+
+printf("square root of %d is =%d\n", numbers,squareRoot );
+    
+    }
+    return 0;
+}
